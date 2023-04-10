48a88ce2e728ca6060cb1fbee8c8667cd002a22d
Morgan Collado <morgan.collado@gmail.com>
2023-04-09 20:10:38 -0500

    implements auth on addresses, people, phones and emails controller

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,6 @@
 class EmailsController < ApplicationController
   before_action :set_email, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /emails or /emails.json
   def index
--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,5 +1,6 @@
 class PhonesController < ApplicationController
   before_action :set_phone, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /phones or /phones.json
   def index
